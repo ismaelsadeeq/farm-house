9d29def74d69d1f57b8f64e14ab0981c6fa866b1
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2021-07-22 23:26:49 +0100

    open api

--- routes/open.api.users.js
+++ routes/open.api.users.js
@@ -3,5 +3,46 @@ var router = express.Router();
 const passport = require('passport');
 const controller = require('../controllers/open.api.user.controller');
 
+router.get('/', 
+  passport.authenticate('jwt',{session:false}),
+  controller.getUser
+);
+
+router.post('/login', 
+  controller.login
+);
+
+router.post('/verify',
+  controller.verifyEmail
+)
+
+router.post('/send-code',
+  controller.sendCode
+)
+router.post('/reset-password',
+  controller.resetPassword
+)
+router.post('/change-password', 
+  passport.authenticate('jwt',{session:false}),
+  controller.changePassword
+);
+router.post('/logout', 
+  passport.authenticate('jwt',{session:false}),
+  controller.logout
+);
+
+router.post('/', 
+  controller.register
+);
+
+router.put('/', 
+  passport.authenticate('jwt',{session:false}),
+  controller.editUser
+);
+
+router.delete('/', 
+  passport.authenticate('jwt',{session:false}),
+  controller.deleteUser
+);
 
 module.exports = router;
